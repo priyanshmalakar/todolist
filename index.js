a2fdf4c077955069d284dd8e7ec54357da7d9137
priyansh malakar <priyanshmalakar33@gmail.com>
2023-01-01 17:22:32 +0530

    change v1.2

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.static("public"))
 
 mongoose.set('strictQuery', false);
 
-mongoose.connect(process.env.URL, () => {
+mongoose.connect(process.env.URL,{useNewUrlParser: true } ,() => {
     console.log("mongodb connected!")
 });
 
